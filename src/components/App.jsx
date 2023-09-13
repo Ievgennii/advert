8591a7198d8f870dbdbfe29fce28cd4e9e03b07d
Ievgennii <simonovievgenii@gmail.com>
2023-09-13 23:48:06 +0300

    added homepage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
 import { lazy } from 'react';
-import { Route, Routes } from 'react-router-dom';
-const NotFound = lazy(() => import('../pages/NotFound/NotFound'));
+import { Navigate, Route, Routes } from 'react-router-dom';
+import HomePage from 'pages/HomePage/HomePage';
+// import SharedLayout from './SharedLayout/SharedLayout';
+
+// const CatalogPage = lazy(() => import('../pages/CatalogPage/CatalogPage'));
+// const FavoritesPage = lazy(() =>
+//   import('../pages/FavoritesPage/FavoritesPage')
+// );
 
 const App = () => {
   return (
     <Routes>
+      <Route path="/" element={<HomePage />} />
       {/* <Route path="/" element={<SharedLayout />}>
-        <Route path="/home" element={<Home />} />
-        <Route path="/catalog" element={<Catalog />} />
-        <Route path="/favorites" element={<Favorites />} />
-      </Route> */}
-      <Route path="/" element={<NotFound />} />
+        <Route path="/catalog" element={<CatalogPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+      </Route>
+      <Route path="*" element={<Navigate to="/" />} /> */}
     </Routes>
   );
 };
