7def07ed994a0036cb0605de4e07d570b0cde03e
Ievgennii <simonovievgenii@gmail.com>
2023-09-16 22:23:35 +0300

    change favorite

--- src/pages/FavoritesPage/FavoritesPage.jsx
+++ src/pages/FavoritesPage/FavoritesPage.jsx
@@ -59,7 +59,9 @@ export default function FavoritesPage() {
 
   return (
     <>
-      <h1 className={css.favTitle}>Favorites</h1>
+      <div className={css.divTitle}>
+        <h1 className={css.favTitle}>Favorites</h1>
+      </div>
       {status === 'fullfield' ? (
         <ul className={css.cardList}>
           {favCards &&
